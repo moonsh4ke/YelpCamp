0b79245e8c984fc7e9a16d95cc467b64112a1e45
Martin Cespedes <mcespedescubillos@gmail.com>
2023-03-17 19:25:53 -0300

    Use heroku port or dev port & added npm start

--- app.js
+++ app.js
@@ -110,6 +110,7 @@ app.use((error, req, res, next) => {
   res.send(error.message);
 })
 
-app.listen(3000, ()=> {
-  console.log('Serving on port 3000')
+const port = process.env.PORT || 3000;
+app.listen(port, ()=> {
+  console.log(`Serving on port ${port}`)
 })
